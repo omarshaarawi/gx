a1fc87ec9f7a61f5bab3c7a9128e1368343b460a
omarshaarawi <omar25shaarawi@gmail.com>
2025-10-12 19:09:31 -0500

    add cobra cli framework and root command

---
+++ cmd/gx/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var version = "dev"
+
+var rootCmd = &cobra.Command{
+	Use:   "gx",
+	Short: "My personal tooling for Go",
+	Version: version,
+}
+
+func init() {
+	rootCmd.SetVersionTemplate(`{{.Version}}`)
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
